905e498333488f4cc2c064eb4b0b95f002803b6f
unknown <phe3no@gmail.com>
2022-10-21 11:51:29 +0200

    first commit, added empty functions, wrote first function witch passes test

---
+++ README.md
@@ -0,0 +1,10 @@
+## The TDDVegetableGarden
+
+An assignment where we calculate the following, among other things:
+
+- cost for crop
+- revenue for crop
+- profit for crop
+- total profit
+
+With and without environment factors
---
+++ farm.js
@@ -0,0 +1,7 @@
+const getYieldForPlant = (plantHarvest) => plantHarvest.yield;
+
+const getYieldForCrop = (cropHarvest) => {};
+
+const getTotalYield = ({ cropsHarvest }) => {};
+
+module.exports = { getYieldForPlant, getYieldForCrop, getTotalYield };
