36d8c81065feaf5b2471a5f01e04adca255168f8
unknown <phe3no@gmail.com>
2022-10-21 17:44:22 +0200

    added the roundingNumberToTwoDecimals function and made changes to each function in test.js, so that each result is returned through this function, all tests passed

--- farm.js
+++ farm.js
@@ -1,3 +1,7 @@
+//this function rounds a number to 2 decimals
+//every function in farm.js returns it's results through this function
+const roundingNumberToTwoDecimals = (number) => Math.round(number * 100) / 100;
+
 //helperfunction for getYieldForPlant, returns expected crop-result, based on eFactor
 //if environmentFactor (eFactor) is 0, wether it is high, medium or low, just return the original value
 const calculateWithEnvironmentFactors = (plantFactor, eFactor, yieldResult) => {
@@ -9,14 +13,15 @@ const calculateWithEnvironmentFactors = (plantFactor, eFactor, yieldResult) => {
   if (eFactor === "high" && plantFactor.high !== 0)
     result = result + (result * plantFactor.high) / 100;
   if (result < 0) return 0;
-  return result;
+  return roundingNumberToTwoDecimals(result);
 };
 
 const getYieldForPlant = (plantHarvest, eFactor) => {
   //JavaScript is a loosely typed language, so you don’t have to specify what type of information will be stored in a variable in advance
   //when no arguments are passed to the parameter 'eFactor', the value will be undefined.
   //in that case no calculations have to be made, so return standard yield
-  if (eFactor === undefined) return plantHarvest.yield;
+  if (eFactor === undefined)
+    return roundingNumberToTwoDecimals(plantHarvest.yield);
 
   let yieldResult = plantHarvest.yield;
   //sunFactor
@@ -37,33 +42,42 @@ const getYieldForPlant = (plantHarvest, eFactor) => {
   }
   //more factors could be added, probably that needs (a) new helperfunction(s)
 
-  return Math.round(yieldResult * 100) / 100;
+  return roundingNumberToTwoDecimals(yieldResult);
 };
 
 const getYieldForCrop = (cropHarvest) =>
-  getYieldForPlant(cropHarvest.crop, cropHarvest.eFactor) *
-  cropHarvest.numCrops;
-
-const getTotalYield = ({ crops }) =>
-  crops.reduce(
-    (previousValue, currentValue) =>
-      previousValue + getYieldForCrop(currentValue),
-    0
+  roundingNumberToTwoDecimals(
+    getYieldForPlant(cropHarvest.crop, cropHarvest.eFactor) *
+      cropHarvest.numCrops
   );
 
-const getCostsForCrop = (crops) => crops.crop.cost * crops.numCrops;
+const getTotalYield = ({ crops }) =>
+  roundingNumberToTwoDecimals(
+    crops.reduce(
+      (previousValue, currentValue) =>
+        previousValue + getYieldForCrop(currentValue),
+      0
+    )
+  );
+
+const getCostsForCrop = (crops) =>
+  roundingNumberToTwoDecimals(crops.crop.cost * crops.numCrops);
 
 const getRevenueForCrop = (crops) =>
-  crops.crop.salePrice * getYieldForCrop(crops);
+  roundingNumberToTwoDecimals(crops.crop.salePrice * getYieldForCrop(crops));
 
 const getProfitForCrop = (crops) =>
-  getRevenueForCrop(crops) - getCostsForCrop(crops);
+  roundingNumberToTwoDecimals(
+    getRevenueForCrop(crops) - getCostsForCrop(crops)
+  );
 
 const getTotalProfit = ({ crops }) =>
-  crops.reduce(
-    (previousValue, currentValue) =>
-      previousValue + getProfitForCrop(currentValue),
-    0
+  roundingNumberToTwoDecimals(
+    crops.reduce(
+      (previousValue, currentValue) =>
+        previousValue + getProfitForCrop(currentValue),
+      0
+    )
   );
 
 module.exports = {
