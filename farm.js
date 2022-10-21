2a6f85a87047029331e9e536862801054c7054a8
unknown <phe3no@gmail.com>
2022-10-21 16:10:31 +0200

    modified the getYieldForPlant function and wrote a helper function for it to avoid duplicate code, the tests passed

--- farm.js
+++ farm.js
@@ -1,4 +1,43 @@
-const getYieldForPlant = (plantHarvest) => plantHarvest.yield;
+//helperfunction for getYieldForPlant, returns expected crop-result, based on eFactor
+//if environmentFactor (eFactor) is 0, wether it is high, medium or low, just return the original value
+const calculateWithEnvironmentFactors = (plantFactor, eFactor, yieldResult) => {
+  let result = yieldResult;
+  if (eFactor === "low" && plantFactor.low !== 0)
+    result = result + (result * plantFactor.low) / 100;
+  if (eFactor === "medium" && plantFactor.medium !== 0)
+    result = result + (result * plantFactor.medium) / 100;
+  if (eFactor === "high" && plantFactor.high !== 0)
+    result = result + (result * plantFactor.high) / 100;
+  if (result < 0) return 0;
+  return result;
+};
+
+const getYieldForPlant = (plantHarvest, eFactor) => {
+  //JavaScript is a loosely typed language, so you don’t have to specify what type of information will be stored in a variable in advance
+  //when no arguments are passed to the parameter 'factor', the value will be undefined.
+  if (eFactor === undefined) return plantHarvest.yield;
+
+  let yieldResult = plantHarvest.yield;
+  //sunFactor
+  if (eFactor.sun) {
+    yieldResult = calculateWithEnvironmentFactors(
+      plantHarvest.factor.sun,
+      eFactor.sun,
+      yieldResult
+    );
+  }
+  //windFactor
+  if (eFactor.wind) {
+    yieldResult = calculateWithEnvironmentFactors(
+      plantHarvest.factor.wind,
+      eFactor.wind,
+      yieldResult
+    );
+  }
+  //more factors could be added, probably it needs a new helperfunction
+
+  return yieldResult;
+};
 
 const getYieldForCrop = (cropHarvest) =>
   cropHarvest.crop.yield * cropHarvest.numCrops;
