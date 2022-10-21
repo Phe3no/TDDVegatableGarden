09c0b41b873afa5edc142198bfbe08f9d9fde9a8
unknown <phe3no@gmail.com>
2022-10-21 12:30:39 +0200

    added code to the getTotalYield function, which passed the tests

--- farm.js
+++ farm.js
@@ -3,6 +3,11 @@ const getYieldForPlant = (plantHarvest) => plantHarvest.yield;
 const getYieldForCrop = (cropHarvest) =>
   cropHarvest.crop.yield * cropHarvest.numCrops;
 
-const getTotalYield = ({ cropsHarvest }) => {};
+const getTotalYield = ({ crops }) =>
+  crops.reduce(
+    (previousValue, currentValue) =>
+      previousValue + getYieldForCrop(currentValue),
+    0
+  );
 
 module.exports = { getYieldForPlant, getYieldForCrop, getTotalYield };
